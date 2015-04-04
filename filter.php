5afc136507e70e6eb854d091aa8fde069c7982d9
ashishag <ashishjuit@yahoo.co.in>
2015-04-04 15:55:44 -0400

    Hey, this is updated search

--- filter.php
+++ filter.php
@@ -3,17 +3,9 @@
     .Ta
     {
         display: table;
-		
-		 font-family: "Times New Roman", Times, serif;
-		 width: 1200px;
-		 padding-top: 200px;
-    
-    padding-bottom: 200px;
-    
-		 border-collapse: collapse;
-		 border: 1px solid black;
-		 text-align:center;
-
+		padding-bottom:10px;
+		padding-top: 10px;
+	    font-family: "Times New Roman", Times, serif;
     }
   
     .Heading
@@ -24,12 +16,15 @@
 		font-size:24px;
 		color:#760113;
 		font-style: italic;
-		font-family: "Times New Roman", Times, serif;
     }
     .Ro
     {
         display: table-row;
+        font-weight: bold;
         text-align: center;
+		font-size:24px;
+
+
 		
     }
     .Ce
@@ -37,31 +32,23 @@
         display: table-cell;
         border: solid;
         border-width: thin;
-        padding-left: px;
-    
-    padding-right: 1px;
+        padding-left: 5px;
+        padding-right: 5px;
 		font-size:20px;
 		width: 150px;
-		height:0px;
-		
+		height:30px;
     }
 </style>
 
 <?php
 include("noside.html");
-
 $lng=$_GET["lng1"];
 $lat=$_GET["lat1"];
 $hum= $_GET["humidity"];
 $wind= $_GET["wind"];
 $tem= $_GET["temperature"];
-$pre=$_GET["pressure"];
-$rf=$_GET["rainfall"];
-$mf=$_GET["date"];
-$yf=$_GET["date"];
-$mt=$_GET["date"];
-$yt=$_GET["date"];
-
+$pre= $_GET["pressure"]
+$rf= $_GET["rainfall"]
 if($hum=='aa' or $hum==NULL)
 	$hum=-100;
 if($wind=='aa' or $wind==NULL)
@@ -72,14 +59,6 @@ if($pre=='aa' or $pre== NULL)
 	$pre=0;
 if($rf=='aa' or $rf== NULL)
 	$rf=0;
-if($mf=='aa' or $mf== NULL)
-	$mf=0;
-if($yf=='aa' or $yf== NULL)
-	$mf=0;
-if($mt=='aa' or $mt== NULL)
-	$mt=0;
-if($yt=='aa' or $yt== NULL)
-	$yt=0;
 
 $lng1=round($lng,2);
 $lat1=round($lat,2);
@@ -93,7 +72,8 @@ $msid="100";
 	 <div class='Ta'>
 	 <div class='Ro'>
 	 <div class='Ce'>
-	 Humidity:  <select id="humidity" name="humidity"  > 
+
+	 Humidity:<select id="humidity" name="humidity"  > 
 				<option value="aa" selected="selected" ></option>
 				<option value="0" >> 0</option>
 				<option value="10" >> 10</option>
@@ -102,8 +82,9 @@ $msid="100";
 				<option value="50"  >> 50</option>
           </select>
 		</div>
+
 		<div class='Ce'>
-	 	 Wind_Speed:<select id="wind" name="wind"   > 
+	 	 Wind Speed:<select id="wind" name="wind"   > 
 		 				<option value="aa" selected="selected" ></option>
 		 <option value="0" >> 0</option>
 				<option value="2" >> 2</option>
@@ -112,6 +93,8 @@ $msid="100";
 				<option value="10" >> 10</option>
           </select>
 		 </div>
+
+
 		<div class='Ce'>
 	 Temperature:<select id="temperature" name="temperature"   > 	
 	 				<option value="aa" selected="selected" ></option>
@@ -123,106 +106,33 @@ $msid="100";
           </select>
 		  </div>
 
-		  <div class='Ce'>
+     	  <div class='Ce'>
 	 Pressure:<select id="pressure" name="pressure"   > 	
 	 				<option value="aa" selected="selected" ></option>
 	  <option value="0" >> 0</option>
-				<option value="800" >> 800</option>
-				<option value="1000" >> 1000</option>
-				<option value="1200" >> 1200</option>
-				<option value="1400" >> 1400</option>
+				<option value="10" >> 10</option>
+				<option value="20" >> 20</option>
+				<option value="50" >> 50</option>
+				<option value="70" >> 70</option>
           </select>
 		  </div>
 
 		  <div class='Ce'>
 	 Rainfall:<select id="rainfall" name="rainfall"   > 	
 	 				<option value="aa" selected="selected" ></option>
-	            <option value="0" >> 0</option>
-	            <option value="0.5">> 0.5</option>
-				<option value="1" >> 1</option>
-				<option value="2" >> 2</option>
-				<option value="5" >> 5</option>
-				<option value="7" >> 7</option>
+	  <option value="0" >> 0</option>
+				<option value="10" >> 10</option>
+				<option value="20" >> 20</option>
+				<option value="50" >> 50</option>
+				<option value="70" >> 70</option>
           </select>
 		  </div>
 
-		  <div class='Ce'>
-	 	 Month From:<select id="date" name="date"   > 
-		 				<option value="aa" selected="selected" ></option>
-		        <option value="JAN" >JANUARY</option>
-				<option value="FEB" >FEBRUARY</option>
-				<option value="MAR" >MARCH</option>
-				<option value="APR" >APRIL</option>
-				<option value="MAY" >MAY</option>
-				<option value="JUN" >JUNE</option>
-				<option value="JUL" >JULY</option>
-				<option value="AUG" >AUGUST</option>
-				<option value="SEP" >SEPTEMBER</option>
-				<option value="OCT" >OCTOBER</option>
-				<option value="NOV" >NOVEMBER</option>
-				<option value="DEC" >DECEMBER</option>
 
-          </select>
-		 </div>
-         <div class='Ce'>
-		 Year From:<select id="date" name="date"   > 
-		 				<option value="aa" selected="selected" ></option>
-		        <option value="04" >2004</option>
-				<option value="05" >2005</option>
-				<option value="06" >2006</option>
-				<option value="07" >2007</option>
-				<option value="08" >2008</option>
-				<option value="09" >2009</option>
-				<option value="10" >2010</option>
-				<option value="11" >2011</option>
-				<option value="12" >2012</option>
-				<option value="13" >2013</option>
-				<option value="14" >2014</option>
-				
-
-          </select>
-		 </div>
-         <div class='Ce'>
-		 Month Till:<select id="date" name="date"   > 
-		 				<option value="aa" selected="selected" ></option>
-		        <option value="JAN" >JANUARY</option>
-				<option value="FEB" >FEBRUARY</option>
-				<option value="MAR" >MARCH</option>
-				<option value="APR" >APRIL</option>
-				<option value="MAY" >MAY</option>
-				<option value="JUN" >JUNE</option>
-				<option value="JUL" >JULY</option>
-				<option value="AUG" >AUGUST</option>
-				<option value="SEP" >SEPTEMBER</option>
-				<option value="OCT" >OCTOBER</option>
-				<option value="NOV" >NOVEMBER</option>
-				<option value="DEC" >DECEMBER</option>
-
-				</select>
-		 </div>
-
-          <div class='Ce'>
-		  Year Till:<select id="date" name="date" > 
-		 				<option value="aa" selected="selected" ></option>
-		        <option value="04" >2004</option>
-				<option value="05" >2005</option>
-				<option value="06" >2006</option>
-				<option value="07" >2007</option>
-				<option value="08" >2008</option>
-				<option value="09" >2009</option>
-				<option value="10" >2010</option>
-				<option value="11" >2011</option>
-				<option value="12" >2012</option>
-				<option value="13" >2013</option>
-				<option value="14" >2014</option>
-				
-
-          </select>
-		 </div>
 
 	 <div class='Ce'> 
-<input type="submit" value="SEARCH">
-	  
+<input type="submit" value="FILTER SEARCH">
+	  </button>
 	  </div> 
 	  </div></div> 
 	  </form>
@@ -259,9 +169,7 @@ $conn= oci_connect("vkhurana","pulsar220", '(DESCRIPTION = (ADDRESS = (PROTOCOL 
        //echo "Connected to foo";
 	  
 	
-		 $b="SELECT * from Climate_data where station_id='".$msid."' and humidity >= ".$hum." and wind_speed >= ".$wind.
-		 " and temperature >= ".$tem." and pressure >= ".$pre." and rainfall >= ".$rf;//."and MONTH(Date) >= ".$mf." AND YEAR(Date) >= ".$yf.
-		 //" and MONTH(Date) <= ".$mt." AND YEAR(Date) <= ".$yt;
+		 $b="SELECT * from Climate_data where station_id='".$msid." and humidity >= ".$hum." and wind_speed >= ".$wind." and temperature >= ".$tem. "and pressure >= ".$pre. "and rainfall >= ".$rf;
 		// echo $b;
 	   $stid = oci_parse($conn,$b);
 	   oci_execute($stid);
@@ -303,5 +211,5 @@ $conn= oci_connect("vkhurana","pulsar220", '(DESCRIPTION = (ADDRESS = (PROTOCOL 
    {
        die("could not connect to foo");
    }
-include("footer.html");
+
 ?>
