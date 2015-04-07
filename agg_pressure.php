a266f8b55afb570e14f6a60fd160d8cbc2b99d35
henfredemars <marsrover7@gmail.com>
2015-04-07 14:27:39 -0400

    Added some new aggregate tools to the tools page

---
+++ agg_pressure.php
@@ -0,0 +1,31 @@
+<?php include("noside.php"); ?>
+<?php include("config.php"); ?>
+
+<h1> Welcome! </h1>
+<p> You are viewing the aggregate pressure tool for station number 
+
+<?php //Intro message
+if (!empty($_GET['station_id'])) {
+  $station_id = (string)$_GET["station_id"];
+} else {
+  $station_id = "722060"; //Gainesville
+}
+echo $station_id;
+?>
+.<h1> Station Summary for the Measurement Period</h1>
+
+<?php
+
+if (!$conn) {
+  echo "<p>Cannot connect to database</p>";
+} else {
+
+  //Print results to the page
+  echo "<img src=\"jpgraph/graph_pressure.php?station_id=$station_id\">";
+} 
+
+//Free unused resources
+oci_close($conn);
+?>
+
+<?php include("footer.html"); ?>
--- tools.php
+++ tools.php
@@ -6,6 +6,23 @@
 
 <form action="agg_temperature.php" method="get">
 <input type="hidden" name="station_id" value="<?php echo $_GET["station_id"]; ?>">
+<input type="submit" value="Aggregate Temperature Tool">
+</form>
+<form action="agg_humidity.php" method="get">
+<input type="hidden" name="station_id" value="<?php echo $_GET["station_id"]; ?>">
+<input type="submit" value="Aggregate Humidity Tool">
+</form>
+<form action="agg_rainfall.php" method="get">
+<input type="hidden" name="station_id" value="<?php echo $_GET["station_id"]; ?>">
+<input type="submit" value="Aggregate Rainfall Tool">
+</form>
+<form action="agg_pressure.php" method="get">
+<input type="hidden" name="station_id" value="<?php echo $_GET["station_id"]; ?>">
+<input type="submit" value="Aggregate Pressure Tool">
+</form>
+<form action="agg_wind.php" method="get">
+<input type="hidden" name="station_id" value="<?php echo $_GET["station_id"]; ?>">
+<input type="submit" value="Aggregate Wind Tool">
 </form>
 
 <h1> Prediction Tools </h1>
