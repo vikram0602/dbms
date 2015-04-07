d5c5dd7569efb7ccee7f45eb5430d1f6b2ab6b7b
henfredemars <marsrover7@gmail.com>
2015-04-07 14:30:50 -0400

    Added button width

--- tools.php
+++ tools.php
@@ -6,23 +6,23 @@
 
 <form action="agg_temperature.php" method="get">
 <input type="hidden" name="station_id" value="<?php echo $_GET["station_id"]; ?>">
-<input type="submit" value="Aggregate Temperature Tool">
+<input type="submit" style="width:500px" value="Aggregate Temperature Tool">
 </form>
 <form action="agg_humidity.php" method="get">
 <input type="hidden" name="station_id" value="<?php echo $_GET["station_id"]; ?>">
-<input type="submit" value="Aggregate Humidity Tool">
+<input type="submit" style="width:500px" value="Aggregate Humidity Tool">
 </form>
 <form action="agg_rainfall.php" method="get">
 <input type="hidden" name="station_id" value="<?php echo $_GET["station_id"]; ?>">
-<input type="submit" value="Aggregate Rainfall Tool">
+<input type="submit" style="width:500px" value="Aggregate Rainfall Tool">
 </form>
 <form action="agg_pressure.php" method="get">
 <input type="hidden" name="station_id" value="<?php echo $_GET["station_id"]; ?>">
-<input type="submit" value="Aggregate Pressure Tool">
+<input type="submit" style="width:500px" value="Aggregate Pressure Tool">
 </form>
 <form action="agg_wind.php" method="get">
 <input type="hidden" name="station_id" value="<?php echo $_GET["station_id"]; ?>">
-<input type="submit" value="Aggregate Wind Tool">
+<input type="submit" style="width:500px" value="Aggregate Wind Tool">
 </form>
 
 <h1> Prediction Tools </h1>
