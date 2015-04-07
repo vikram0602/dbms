948f997aca2c9a3393a6fd8de7d2842cc215804e
henfredemars <marsrover7@gmail.com>
2015-04-07 14:15:56 -0400

    Logout function when using the noside template

--- tools.php
+++ tools.php
@@ -4,8 +4,9 @@
 
 <h1> Aggregate Tools </h1>
 
-<form action="agg_temperature.php" method="post">
-<input type="hidden" value="<?php  ?>"
+<form action="agg_temperature.php" method="get">
+<input type="hidden" name="station_id" value="<?php echo $_GET["station_id"]; ?>">
+</form>
 
 <h1> Prediction Tools </h1>
 
